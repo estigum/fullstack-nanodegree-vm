5eb387d5381f569245c5574c7005be31e9dbcce6
estigum <estigum@gmail.com>
2015-10-25 20:57:35 -0400

    I updarted the files

--- vagrant/tournament/swisstournament.py
+++ vagrant/tournament/swisstournament.py
@@ -1,42 +1,34 @@
+"""
+This is the SwissTournament module
+It has a main class for running tournaments
+"""
 from random import randint
 import tournament
 
-class SwissTournamentResult(object):
-
-    def __init__(self):
-        self.winner = None
-        self.loser = None
-
-    def set_winner(self, winner):
-        self.winner = winner
-
-    def set_looser(self, loser):
-
-        self.loser = loser
-
-    def get_winner(self):
-
-        return self.winner
-
-    def get_loser(self):
-
-        return self.loser
-'''
-
-'''
 class SwissTournament(object):
+    """
+    This is the SwissTournament class
+    It will run one or more tournament
+    """
 
     def __init__(self, supportodd):
-
+        """
+        This is the constructor.  It takes one parameter if we will support odd lists
+        :param supportodd:
+        :return:
+        """
         self.players = dict()
         self.swisspairing = []
         self.numplayers = None
-        self.results = dict()
-        self.score = dict()
         self.supportodd = supportodd
         self.db = tournament.connect()
 
     def load_players_from_file(self,filename):
+        """
+        This will load all the players from file and save them to the database
+        :param filename:
+        :return:
+        """
 
         tournament.deletePlayers(self.db)
         players = open(filename,"r")
@@ -46,11 +38,20 @@ class SwissTournament(object):
         players.close()
 
     def number_of_players(self):
+        """
+        This sets the number of players in the tournament
+        :return:
+        """
         players = tournament.countPlayers(self.db)
         print "Number of players " + str(players[0])
         self.numplayers = players[0]
 
     def load_players_from_db(self):
+        """
+        This will load the players from the database and setup the first pairing.
+        After this it will get them from the database
+        :return:
+        """
 
         numpairings = int(self.numplayers/2) + (self.numplayers%2)
 
@@ -77,25 +78,39 @@ class SwissTournament(object):
                     pairing += 1
 
     def play_round(self, tournament_id,current_round):
+        """
+        This will play a given round in a tournament and report the match to the database.
+        :param tournament_id:
+        :param current_round:
+        :return:
+        """
 
-            for parings in self.swisspairing:
-                if len(parings) == 2:
-                    aval = randint(0,9)
-                    bval = randint(0,9)
-                    #print "Round #" + str(current_round) + " and match between " + parings[0][1] + " and "  + parings[1][1]
-                    if aval > bval:
-                        tournament.reportMatch(parings[0][0],parings[1][0],tournament_id,current_round,self.db)
-                    else:
-                        tournament.reportMatch(parings[1][0],parings[0][0],tournament_id,current_round,self.db)
+        for parings in self.swisspairing:
+            if len(parings) == 2:
+                aval = randint(0,9)
+                bval = randint(0,9)
+                if aval > bval:
+                    tournament.reportMatch(parings[0][0],parings[1][0],tournament_id,current_round,self.db)
+                else:
+                    tournament.reportMatch(parings[1][0],parings[0][0],tournament_id,current_round,self.db)
 
 def printStandings(results, round):
-
+    """
+    This will print the results for a given round based on the results passed in.
+    :param results:
+    :param round:
+    :return:
+    """
     print "Results for Round " + str(round)
     print("%5s %-20s %10s %10s" % ("id","Name","Wins","Rounds"))
     for result in results:
         print("%5d %-20s %10d %10d" % (result[0], result[2], result[1], result[3]))
 
 def main():
+    """
+    This is the main function that sets up the tournament
+    :return:
+    """
     myswiss = SwissTournament(False)
     tournament.deleteMatches(myswiss.db,1)
     tournament.deleteSwissTournaments(myswiss.db)
