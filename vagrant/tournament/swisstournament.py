9c3788bf0716768a9c379b5c1f46bb38eca4446e
estigum <estigum@gmail.com>
2015-11-09 19:37:20 -0500

    Added the ability to log the results to file. This was needed because in vagrant can't bring up web page. Also added exception handling.

--- vagrant/tournament/swisstournament.py
+++ vagrant/tournament/swisstournament.py
@@ -32,7 +32,7 @@ class SwissTournament(object):
     It will run one or more tournament
     """
 
-    def __init__(self, supportodd, logger):
+    def __init__(self, supportodd, logger, output_fd):
         """
         This is the constructor.  It takes one parameter if
         we will support odd lists
@@ -45,6 +45,7 @@ class SwissTournament(object):
         self.supportodd = supportodd
         self.database = tournament.connect()
         self.logger = logger
+        self.output_fd = output_fd
 
     def load_players_from_file(self, filename):
         """
@@ -109,12 +110,13 @@ class SwissTournament(object):
         url = os.path.abspath(html_file.name)
         webbrowser.open('file://' + url, new=2) # open in a new tab, if possible
 
-    def start_tournament(self, tournament_name):
+    def start_tournament(self, tournament_name, web_support):
         """
         This method is to start a new tournament.
         :param tournament_name:
         :return:
         """
+        self.output_fd.write("Starting New Tournament!!!  Tournament Name = " + tournament_name + "\n")
         html_file = create_html_page(tournament_name)
 
         self.load_players_from_db()
@@ -123,12 +125,12 @@ class SwissTournament(object):
                                                         self.database)
 
         results = tournament.playerStandings(tournament_id, self.database)
-        print_standings(results, 1, self.logger)
+        print_standings(results, 1, self.output_fd)
 
         self.play_round(tournament_id, 1)
         tournament.updateSwissTournamentRound(tournament_id, 1, self.database)
         results = tournament.playerStandings(tournament_id, self.database)
-        print_standings(results, 1, self.logger)
+        print_standings(results, 1, self.output_fd)
         print_html_standings(html_file,results,1)
 
         past_matches = tournament.getPastMatchesForTournament(self.database,
@@ -139,7 +141,7 @@ class SwissTournament(object):
         self.play_round(tournament_id, 2)
         tournament.updateSwissTournamentRound(tournament_id, 2, self.database)
         results = tournament.playerStandings(tournament_id, self.database)
-        print_standings(results, 2, self.logger)
+        print_standings(results, 2, self.output_fd)
         print_html_standings(html_file,results,2)
 
         past_matches = tournament.getPastMatchesForTournament(self.database,
@@ -150,7 +152,7 @@ class SwissTournament(object):
         self.play_round(tournament_id, 3, past_matches)
         tournament.updateSwissTournamentRound(tournament_id, 3, self.database)
         results = tournament.playerStandings(tournament_id, self.database)
-        print_standings(results, 3, self.logger)
+        print_standings(results, 3, self.output_fd)
         print_html_standings(html_file,results,3)
 
         past_matches = tournament.getPastMatchesForTournament(self.database,
@@ -161,13 +163,14 @@ class SwissTournament(object):
         self.play_round(tournament_id, 4, past_matches)
         tournament.updateSwissTournamentRound(tournament_id, 4, self.database)
         results = tournament.playerStandings(tournament_id, self.database)
-        print_standings(results, 4, self.logger)
+        print_standings(results, 4, self.output_fd)
         print_html_standings(html_file,results,4)
 
         html_file.write("</div>\n</body>\n</html>\n")
         html_file.close()
         url = os.path.abspath(html_file.name)
-        webbrowser.open('file://' + url, new=2) # open in a new tab, if possible
+        if web_support == "True":
+            webbrowser.open('file://' + url, new=2) # open in a new tab, if possible
 
     def play_round(self, tournament_id, current_round, past_matches=None):
         """
@@ -228,7 +231,7 @@ def print_html_standings(html_file, results, current_round):
         html_file.write("</tr>\n")
     html_file.write("</table>\n")
 
-def print_standings(results, current_round, logger):
+def print_standings(results, current_round, output_fd):
     """
     This will print the results for a given round
     based on the results passed in.
@@ -236,39 +239,69 @@ def print_standings(results, current_round, logger):
     :param round:
     :return:
     """
-    logger.info("Results for Round " + str(current_round))
-    logger.info("%5s %-20s %10s %10s" % ("id", "Name", "Wins", "Rounds"))
+    output_fd.write("Results for Round " + str(current_round) + "\n")
+    output_fd.write("%5s %-20s %10s %10s" % ("id", "Name", "Wins", "Rounds") + "\n")
     for result in results:
-        logger.info("%5d %-20s %10d %10d" % (result[0], result[1],
-                                       result[2], result[3]))
+        output_fd.write("%5d %-20s %10d %10d" % (result[0], result[1],
+                                       result[2], result[3]) + "\n")
 
 def main():
     """
     This is the main function that sets up the tournament
     :return:
     """
+    try:
+        if len(sys.argv) < 2:
+            print "python swisstournament <configfile>"
+            return
+        config_parser = ConfigParser.ConfigParser()
+        config_parser.read(sys.argv[1])
+        logger = swisslogger.get_logger()
+
+        if not config_parser.has_option("logging", "mode"):
+            raise Exception("No mode setup for logging section in configuration file")
+
+        swisslogger.set_logger_mode(logger, config_parser.get("logging", "mode"))
+
+        if not config_parser.has_option("run_mode", "output_file"):
+            raise Exception("No output_file in the run_mode section of the configuration file")
+
+        output_file_name = config_parser.get("run_mode", "output_file")
+        output_fd = open(output_file_name,"w")
+
+        if not config_parser.has_option("run_mode", "web_support"):
+            web_support = "False"
+        else:
+            web_support = config_parser.get("run_mode", "web_support")
+        print web_support
+
+        myswiss = SwissTournament(False,logger, output_fd)
+        tournament.deleteMatches(myswiss.database)
+        tournament.deleteSwissTournaments(myswiss.database)
+
+        if not config_parser.has_option("run_mode", "player_file"):
+            raise Exception("No player_file specified in run_mode section of confguration file")
+
+        player_file_name = config_parser.get("run_mode", "player_file")
+        myswiss.load_players_from_file(player_file_name)
+        myswiss.number_of_players()
 
 
-    if len(sys.argv) < 2:
-        print "python swisstournament <configfile>"
-        return
-    config_parser = ConfigParser.ConfigParser()
-    config_parser.read(sys.argv[1])
-    logger = swisslogger.get_logger()
-    swisslogger.set_logger_mode(logger, config_parser.get("logging", "mode"))
+        if not config_parser.has_option("run_mode", "tournaments"):
+           raise Exception("No tournaments specified in run_mode section of configuration file")
 
-    myswiss = SwissTournament(False,logger)
-    tournament.deleteMatches(myswiss.database)
-    tournament.deleteSwissTournaments(myswiss.database)
+        tournaments = config_parser.get("run_mode", "tournaments")
 
-    player_file_name = config_parser.get("run_mode", "player_file")
-    myswiss.load_players_from_file(player_file_name)
-    myswiss.number_of_players()
+        temp = tournaments.split(",")
+        for next_tournament in temp:
+            myswiss.start_tournament(next_tournament, web_support)
 
-    tournaments = config_parser.get("run_mode", "tournaments")
-    temp = tournaments.split(",")
-    for next_tournament in temp:
-        myswiss.start_tournament(next_tournament)
-    myswiss.overall_tournament_results()
+        if web_support == "True":
+            myswiss.overall_tournament_results()
+        output_fd.close()
+
+    except Exception, e:
+        logger.error("Error: " + e.message)
+
 if __name__ == "__main__":
     main()
